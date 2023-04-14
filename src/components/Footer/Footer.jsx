407159dcbf9104e305342a6c5eef5e90e165f0a6
NazarFedus <simpleprograming7@gmail.com>
2023-04-14 18:05:58 +0100

    footer developing

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function Footer(){
+     return(
+          <footer>
+               <div className='footer__content'>
+                    <div className=''></div>
+               </div>
+          </footer>
+     );
+}
