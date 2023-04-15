381ff5454feef6db4b8fdc92a4182cdd88216722
Hermann <edgar.poe1994@gmail.com>
2023-04-15 12:01:56 +0300

    add finall html and classes3

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
-<<<<<<< HEAD
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TopHeader from './components/Header/components/TopHeader'
-=======
+
 import './style.scss'
->>>>>>> 6668011bd044f427d578896cadb02d25e8020b87
+
 
 function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <TopHeader />
-=======
-    
->>>>>>> 6668011bd044f427d578896cadb02d25e8020b87
+
     </div>
   )
 }
