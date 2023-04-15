d95be3e84d83a5948e4386213bfda6955ea755dd
Hermann <edgar.poe1994@gmail.com>
2023-04-15 11:37:44 +0300

    add finall html and classes

--- src/components/Header/components/TopHeader.jsx
+++ src/components/Header/components/TopHeader.jsx
@@ -4,25 +4,26 @@ import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
 
 export default function TopHeader () {
     return (
-        <>
+        <div className={ styles.header_wrap }>
             <div className={ styles.header }>
-                <div>
-                    <p>Mon-Thu: <span>9:00 AM - 5:30 PM</span> </p>
+                <div className={ styles.header__workTime }>
+                    <p>Mon-Thu: <span>9:00 AM - 5:30 PM</span></p>
                 </div>
-                <div>
-                    <p>Visit our showroom in 1234 Street Adress City Address, 1234  Contact Us</p>
+                <div className={ styles.header__adress }>
+                    <p>Visit our showroom in 1234 Street Adress City Address, 1234</p>
+                    <a href="#">Contact Us</a>
                 </div>
-                <div>
-                    <div>
+                <div className={ styles.header__social } >
+                    <div className={ styles.header__social_tel }>
                         <p>Call Us:</p>
-                        <a href="tel:0012345678">(00) 1234 5678</a>
+                        <a className={ styles.header__social_tel_link } href="tel:0012345678">(00) 1234 5678</a>
                     </div>
-                    <div>
-                        <a href=""> <AiFillFacebook /> </a>
-                        <a href=""> <AiFillInstagram /> </a>
+                    <div >
+                        <a className={ styles.header__social_icons } href="#"> <AiFillFacebook /> </a>
+                        <a className={ styles.header__social_icons } href="#"> <AiFillInstagram /> </a>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
