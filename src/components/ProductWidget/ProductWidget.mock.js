56cb80d23fab6c4bee1b421f3a1b05864ab499c6
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-04-15 18:28:29 +0300

    add prettier file

--- src/components/ProductWidget/ProductWidget.mock.js
+++ src/components/ProductWidget/ProductWidget.mock.js
@@ -1,82 +1,88 @@
 export const data = [
-    {
-        id: 1,
-        title: "wkrjgnw",
-        price: 467.00,
-        fullPrice:  789.00,
-        isStock: true,
-        rating: 2,
-        reviews: 105,
-        image: 'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
-    },
-    {
-        id: 2,
-        title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On... ",
-        price: 467.00,
-        fullPrice:  5234.00,
-        isStock: false,
-        rating: 0,
-        reviews: 4532,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDEHhK_HmCCccz9NlypnWtL5GTtUo1H2Vkqg&usqp=CAU'
-    },
-    {
-        id: 3,
-        title: "EX DISPLAY : MSI Pro 16 fewfwefw.6 MULTITOUCH Algergn...",
-        price: 764.00,
-        fullPrice:  789.00,
-        isStock: true,
-        rating: 3,
-        reviews: 345,
-        image: 'https://images.prom.ua/3924504148_w640_h640_3924504148.jpg'
-    },
-    {
-        id: 4,
-        title: "gergergerge grjkg w ewew ew  efhw w",
-        price: 5432.00,
-        fullPrice:  5342.00,
-        isStock: false,
-        rating: 5,
-        reviews: 135,
-        image: 'https://itc.ua/wp-content/uploads/2022/10/TRS-80.jpg'
-    },
-    {
-        id: 5,
-        title: "few few fwefew fwefwef wefw",
-        price: 534.00,
-        fullPrice:  5432.00,
-        isStock: true,
-        rating: 1,
-        reviews: 8546,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Fvle36kwP5vxDROnvGRbtUy9nu7UnNuSht8k3CpSxxCjzKCHBDFE5BEDPj9fbL8WDJ4&usqp=CAU'
-    },
-    {
-        id: 6,
-        title: "wkrf wefwefj few  fwe ",
-        price: 53425.00,
-        fullPrice:  54325.00,
-        isStock: false,
-        rating: 4,
-        reviews: 563,
-        image: 'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
-    },
-    {
-        id: 7,
-        title: "wkrjf weewf wef ew fwefgnw",
-        price: 5345.00,
-        fullPrice:  3423.00,
-        isStock: true,
-        rating: 5,
-        reviews: 7585,
-        image: 'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
-    },
-    {
-        id: 8,
-        title: "f wefwfewfw f gewgweg e2twewfwef ",
-        price: 234.00,
-        fullPrice:  524.00,
-        isStock: true,
-        rating: 3,
-        reviews: 105,
-        image: 'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
-    }
+  {
+    id: 1,
+    title: 'wkrjgnw',
+    price: 467.0,
+    fullPrice: 789.0,
+    isStock: true,
+    rating: 2,
+    reviews: 105,
+    image:
+      'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
+  },
+  {
+    id: 2,
+    title: 'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On... ',
+    price: 467.0,
+    fullPrice: 5234.0,
+    isStock: false,
+    rating: 0,
+    reviews: 4532,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDEHhK_HmCCccz9NlypnWtL5GTtUo1H2Vkqg&usqp=CAU'
+  },
+  {
+    id: 3,
+    title: 'EX DISPLAY : MSI Pro 16 fewfwefw.6 MULTITOUCH Algergn...',
+    price: 764.0,
+    fullPrice: 789.0,
+    isStock: true,
+    rating: 3,
+    reviews: 345,
+    image: 'https://images.prom.ua/3924504148_w640_h640_3924504148.jpg'
+  },
+  {
+    id: 4,
+    title: 'gergergerge grjkg w ewew ew  efhw w',
+    price: 5432.0,
+    fullPrice: 5342.0,
+    isStock: false,
+    rating: 5,
+    reviews: 135,
+    image: 'https://itc.ua/wp-content/uploads/2022/10/TRS-80.jpg'
+  },
+  {
+    id: 5,
+    title: 'few few fwefew fwefwef wefw',
+    price: 534.0,
+    fullPrice: 5432.0,
+    isStock: true,
+    rating: 1,
+    reviews: 8546,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Fvle36kwP5vxDROnvGRbtUy9nu7UnNuSht8k3CpSxxCjzKCHBDFE5BEDPj9fbL8WDJ4&usqp=CAU'
+  },
+  {
+    id: 6,
+    title: 'wkrf wefwefj few  fwe ',
+    price: 53425.0,
+    fullPrice: 54325.0,
+    isStock: false,
+    rating: 4,
+    reviews: 563,
+    image:
+      'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
+  },
+  {
+    id: 7,
+    title: 'wkrjf weewf wef ew fwefgnw',
+    price: 5345.0,
+    fullPrice: 3423.0,
+    isStock: true,
+    rating: 5,
+    reviews: 7585,
+    image:
+      'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
+  },
+  {
+    id: 8,
+    title: 'f wefwfewfw f gewgweg e2twewfwef ',
+    price: 234.0,
+    fullPrice: 524.0,
+    isStock: true,
+    rating: 3,
+    reviews: 105,
+    image:
+      'https://mevs.com.ua/image/cache/3286564827_kompyuter-dlya-igr-500x500.jpg'
+  }
 ]
