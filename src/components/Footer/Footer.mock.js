6cab6b4688eb0dadb03434215d97c9186c29d3d8
NazarFedus <simpleprograming7@gmail.com>
2023-04-15 23:28:04 +0100

    add footerdata (Footer.mock)

--- src/components/Footer/Footer.mock.js
+++ src/components/Footer/Footer.mock.js
@@ -1,3 +1,80 @@
-export const data = {
-     
-}
+export const links = [
+     {
+          title: 'Information',
+          links: [
+                    {title: 'About Us', link: ''},
+                    {title: 'About Zip', link: ''},
+                    {title: 'Privacy Policy', link: ''},
+                    {title: 'Search', link: ''},
+                    {title: 'Terms', link: ''},
+                    {title: 'Orders and Returns', link: ''},
+                    {title: 'Contact Us', link: ''},
+                    {title: 'Advanced Search', link: ''},
+                    {title: 'Newsletter Subscription', link: ''}
+               ]
+     },
+     {
+          title: 'PC Parts',
+          links: [
+                    {title: 'CPUS', link: ''},
+                    {title: 'Add On Cards', link: ''},
+                    {title: 'Hard Drives (Internal)', link: ''},
+                    {title: 'Graphic Cards', link: ''},
+                    {title: 'Keyboards / Mice', link: ''},
+                    {title: 'Cases / Power Supplies / Cooling', link: ''},
+                    {title: 'RAM (Memory)', link: ''},
+                    {title: 'Software', link: ''},
+                    {title: 'Speakers / Headsets', link: ''},
+                    {title: 'Motherboards', link: ''}
+               ]
+     },
+     {
+          title: 'Desktop PCs',
+          links: [
+                    {title: 'Custom PCs', link: ''},
+                    {title: 'Servers', link: ''},
+                    {title: 'MSI All-In-One PCs', link: ''},
+                    {title: 'HP/Compaq PCs', link: ''},
+                    {title: 'ASUS PCs', link: ''},
+                    {title: 'Tecs PCs', link: ''}
+               ]
+     },
+     {
+          title: 'Laptops',
+          links: [
+                    {title: 'Evryday Use Notebooks', link: ''},
+                    {title: 'MSI Workstation Series', link: ''},
+                    {title: 'MSI Prestige Series', link: ''},
+                    {title: 'Tablets and Pads', link: ''},
+                    {title: 'Netbooks', link: ''},
+                    {title: 'Infinity Gaming Notebooks', link: ''}
+               ]
+     },
+]
+
+export const addressData = [
+     {
+          title: 'Address',
+          value: '1234 Street Adress City Address, 1234'
+     },
+     {
+          title: 'Phones',
+          value: '(00) 1234 5678'
+     },
+     {
+          title: 'We are open',
+          value: 'Monday-Thursday: 9:00 AM - 5:30 PM'
+     },
+     {
+          title: 'Friday',
+          value: '9:00 AM - 6:00 PM'
+     },
+     {
+          title: 'Saturday',
+          value: '11:00 AM - 5:00 PM'
+     },
+     {
+          title: 'E-mail',
+          value: 'shop@email.com'
+     }
+]
