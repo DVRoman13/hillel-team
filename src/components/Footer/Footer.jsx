cae86bbd617853919688181b3c1936fda829944b
NazarFedus <simpleprograming7@gmail.com>
2023-04-16 11:38:12 +0100

    add footerBottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,14 @@
 import styles from './Footer.module.scss'
-
+import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
 import {links} from './Footer.mock'
+
+import paypal from './icons/paypal.png';
+import visa from './icons/visa.png';
+import maestro from './icons/maestro.png';
+import discover from './icons/discover.png';
+import americanexpress from './icons/american-express.png';
+
+
 
 import InfoBox from './components/InfoBox/InfoBox'
 
@@ -29,7 +37,23 @@ export default function Footer() {
         <div className={styles.footer__contentMiddle}>
           {links.map(item => <InfoBox key={item.id} {...item} />)}
         </div>
-        <div className={styles.footer__content_bottom}></div>
+        <div className={styles.footer__content_bottom}>
+          <div className={styles.content_bottom__icons}>
+               <a className={ styles.bottom_icons} href="#"> <AiFillFacebook className={styles.social_icon}/> </a>
+               <a className={ styles.bottom_icons} href="#"> <AiFillInstagram className={styles.social_icon}/> </a>
+          </div>
+          <div className={styles.content_bottom__cards}>
+               <div className={styles.footer__card_icon}><img src={paypal} alt="paypal" /></div>
+               <div className={styles.footer__card_icon}><img src={visa} alt="visa" /></div>
+               <div className={styles.footer__card_icon}><img src={maestro} alt="maestro" /></div>
+               <div className={styles.footer__card_icon}><img src={discover} alt="discover" /></div>
+               <div className={styles.footer__card_icon}><img src={americanexpress} alt="americanexpress" /></div>
+
+          </div>
+          <div className={styles.content_bottom__text}>
+               <p>Copyright © 2020 Shop Pty. Ltd.</p>
+          </div>
+        </div>
       </div>
     </footer>
   )
