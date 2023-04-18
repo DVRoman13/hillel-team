39d98a35fbb902f591fcc7d2e7ff59e585cc8a90
Bessonov Ilya <illybesson3@gmail.com>
2023-04-18 17:29:04 +0430

    News(finished)_part2.2

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,161 +1,20 @@
 import classes from './News.module.scss'
+import NewsCard from './components/NewsCard'
 const News = () => {
   return (
     <>
       <div className={classes.news}>
         <div className={classes.news__conteiner}>
           <div className={classes.news__display}>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f'
-                  alt='news'
-                />
-              </div>
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/premium-photo/computer-sitting-on-top-of-a-field-of-flowers-generative-ai_733139-4904.jpg?w=1380'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e'            alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/premium-photo/microphone-and-laptop-on-a-wooden-table-generative-ai_634053-4453.jpg?w=1380'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/free-photo/business-chart-visual-graphics-report-concept_53876-120324.jpg?w=1060&t=st=1681673863~exp=1681674463~hmac=e0a2b17f4f9a3218b9c535cbd0c88ef168529b501df8a186535e700bd14be092'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/premium-photo/close-up-view-of-keyboard-in-video-game-room-entertaining-industry_308072-2068.jpg?w=1380'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/premium-photo/a-laptop-with-a-fire-on-it-that-is-burning_391052-12825.jpg?w=1380'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
-            <div className={classes.news__display__card}>
-              <div className={classes.news__images__wrap}>
-                <img
-                  src='https://img.freepik.com/premium-photo/home-workplace-with-a-beautiful-computer-with-blue-light-and-a-red-screen_245974-6519.jpg?w=1380'
-                  alt='news'
-                />
-              </div>
-
-              <span className={classes.news__text__about}>
-                If you’ve recently made a desktop PC or laptop purchase, you
-                might want to consider adding peripherals to enhance your home
-                office setup, your gaming rig, or your business workspace...
-              </span>
-              <span className={classes.news__text__about__data}>
-                01.09.2020
-              </span>
-            </div>
+            <NewsCard src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f' />
+            <NewsCard src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f' />
+            <NewsCard src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0' />
+            <NewsCard src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0' />
+            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
+            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
+            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
+            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
+            <NewsCard src='https://img.freepik.com/premium-photo/home-workplace-with-a-beautiful-computer-with-blue-light-and-a-red-screen_245974-6519.jpg?w=1380' />
           </div>
         </div>
       </div>
