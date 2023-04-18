6ff7763a09ce596ec8c46b9819e42ed12bdba738
Bessonov Ilya <illybesson3@gmail.com>
2023-04-18 23:01:31 +0430

    News(finished)_part2.3

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -6,15 +6,42 @@ const News = () => {
       <div className={classes.news}>
         <div className={classes.news__conteiner}>
           <div className={classes.news__display}>
-            <NewsCard src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f' />
-            <NewsCard src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f' />
-            <NewsCard src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0' />
-            <NewsCard src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0' />
-            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
-            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
-            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
-            <NewsCard src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e' />
-            <NewsCard src='https://img.freepik.com/premium-photo/home-workplace-with-a-beautiful-computer-with-blue-light-and-a-red-screen_245974-6519.jpg?w=1380' />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/closeup-of-a-computer-a-cup-of-coffee-a-vase-of-flowers-and-more-on-a-white-desk-indoors_181624-31240.jpg?w=1380&t=st=1681673448~exp=1681674048~hmac=1e2c460a1cad03f1734b88417a6f655cc54dd86342e1f23b776469d59d93429f'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/person-working-on-animation-porject_23-2149269875.jpg?w=1380&t=st=1681757372~exp=1681757972~hmac=fc9dd4201b6085b09ae92036e8934187112c3c9d26f103bb652ab34c1f6bf4f0'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/free-photo/gaming-setup-arrangement-high-angle_23-2149680236.jpg?w=1380&t=st=1681757420~exp=1681758020~hmac=ff69ccbab1267e60c2277614d816a31f0bae56d851b1c1135a019fa599025c7e'
+            />
+            <NewsCard
+              text='If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...'
+              src='https://img.freepik.com/premium-photo/home-workplace-with-a-beautiful-computer-with-blue-light-and-a-red-screen_245974-6519.jpg?w=1380'
+            />
           </div>
         </div>
       </div>
--- src/components/News/components/NewsCard.jsx
+++ src/components/News/components/NewsCard.jsx
@@ -7,9 +7,7 @@ const NewsCard = props => {
           <img src={props.src} alt='news' />
         </div>
         <span className={classes.news__text__about}>
-          If you’ve recently made a desktop PC or laptop purchase, you might
-          want to consider adding peripherals to enhance your home office setup,
-          your gaming rig, or your business workspace...
+        {props.text}
         </span>
         <span className={classes.news__text__about__data}>01.09.2020</span>
       </div>
