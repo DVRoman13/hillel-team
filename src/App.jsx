8ba3c7c0b00969454d539683511fc289e5b65582
Bessonov Ilya <illybesson3@gmail.com>
2023-04-19 18:50:28 +0430

    News(finished)part_1

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './style.scss'
 import TopHeader from './components/common/Header/components/TopHeader'
 import ProductWidget from './components/ProductWidget/ProductWidget'
 import Footer from './components/common/Footer/Footer'
+import News from './components/common/News/News'
 
 function App() {
   return (
     <div className='App'>
       <TopHeader />
       <ProductWidget title='New Products' />
+      <News/>
       <Footer />
     </div>
   )
