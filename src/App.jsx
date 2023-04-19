131d1e78e9e99029289327657fc38a8299396a96
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-04-19 14:23:47 +0300

    add script prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import './style.scss'
 
 import TopHeader from './components/common/Header/components/TopHeader'
-import ProductWidget from "./components/ProductWidget/ProductWidget";
+import ProductWidget from './components/ProductWidget/ProductWidget'
 import Footer from './components/common/Footer/Footer'
 
-
 function App() {
-
-    return (
-        <div className="App">
-            <TopHeader/>
-            <ProductWidget title="New Products"/>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className='App'>
+      <TopHeader />
+      <ProductWidget title='New Products' />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
--- src/components/common/Header/components/TopHeader.jsx
+++ src/components/common/Header/components/TopHeader.jsx
@@ -1,29 +1,38 @@
 import styles from './TopHeader.module.scss'
-import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
+import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai'
 
-
-export default function TopHeader () {
-    return (
-        <div className={ styles.header_wrap }>
-            <div className={ styles.header }>
-                <div className={ styles.header__workTime }>
-                    <p>Mon-Thu: <span>9:00 AM - 5:30 PM</span></p>
-                </div>
-                <div className={ styles.header__adress }>
-                    <p>Visit our showroom in 1234 Street Adress City Address, 1234</p>
-                    <a href="#">Contact Us</a>
-                </div>
-                <div className={ styles.header__social } >
-                    <div className={ styles.header__social_tel }>
-                        <p>Call Us:</p>
-                        <a className={ styles.header__social_tel_link } href="tel:0012345678">(00) 1234 5678</a>
-                    </div>
-                    <div >
-                        <a className={ styles.header__social_icons } href="#"> <AiFillFacebook /> </a>
-                        <a className={ styles.header__social_icons } href="#"> <AiFillInstagram /> </a>
-                    </div>
-                </div>
-            </div>
+export default function TopHeader() {
+  return (
+    <div className={styles.header_wrap}>
+      <div className={styles.header}>
+        <div className={styles.header__workTime}>
+          <p>
+            Mon-Thu: <span>9:00 AM - 5:30 PM</span>
+          </p>
         </div>
-    )
+        <div className={styles.header__adress}>
+          <p>Visit our showroom in 1234 Street Adress City Address, 1234</p>
+          <a href='#'>Contact Us</a>
+        </div>
+        <div className={styles.header__social}>
+          <div className={styles.header__social_tel}>
+            <p>Call Us:</p>
+            <a className={styles.header__social_tel_link} href='tel:0012345678'>
+              (00) 1234 5678
+            </a>
+          </div>
+          <div>
+            <a className={styles.header__social_icons} href='#'>
+              {' '}
+              <AiFillFacebook />{' '}
+            </a>
+            <a className={styles.header__social_icons} href='#'>
+              {' '}
+              <AiFillInstagram />{' '}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
