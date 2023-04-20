61636a5dd646ccf9c18eb187d7728c7761d42037
NazarFedus <simpleprograming7@gmail.com>
2023-04-20 12:58:42 +0100

    ShoppingList full structure

--- src/pages/ShoppingCart/components/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingCart/components/ShoppingList/ShoppingList.jsx
@@ -1,11 +1,18 @@
 import styles from './ShoppingList.module.scss'
+import React, {useState, useEffect} from 'react';
+
 
 import { data } from '../../../../components/ProductWidget/ProductWidget.mock'
 import ProductCard from './ProductCard/ProductCard';
 
 export default function ShoppingList(){
 
-     console.log(data)
+     const [list, setList] = useState([])
+
+     useEffect(() => {
+          setList(data)
+     }, [])
+
 
      return(
           <div className={styles.shopping_list__container}>
@@ -16,12 +23,14 @@ export default function ShoppingList(){
                     <h6>Subtotal</h6>
                </div>
                <div className={styles.container__main_block}>
-                    {data.map(item => <ProductCard product={item} key={item.id}></ProductCard>)}
+                    {list.map(item => <ProductCard product={item} key={item.id}></ProductCard>)}
                </div>
                <div className={styles.container__buttons_block}>
-                    <button>Continue Shopping</button>
-                    <button>Clear Shopping Cart</button>
-                    <button>Update Shopping Cart</button>
+                    <div className={styles.buttons_block_right}>
+                         <button className={styles.light}>Continue Shopping</button>
+                         <button className={styles.dark}>Clear Shopping Cart</button>
+                    </div>
+                    <button className={styles.dark}>Update Shopping Cart</button>
                </div>
           </div>
      );
