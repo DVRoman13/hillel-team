995c8e3de3a5af1cd2726e934a6980a8e8105f40
NazarFedus <simpleprograming7@gmail.com>
2023-04-21 00:51:46 +0100

    add SummaryForm

--- src/pages/ShoppingCart/components/ShoppingSummary/ShoppingSummary.jsx
+++ src/pages/ShoppingCart/components/ShoppingSummary/ShoppingSummary.jsx
@@ -1,9 +1,37 @@
 import styles from './ShoppingSummary.module.scss'
 
+import React, {useState} from 'react';
+
+import up from './icons/up.svg'
+import down from './icons/down.svg'
+
+import SummaryForm from './components/SummaryForm/SummaryForm';
+
 export default function ShoppingSummary(){
+
+     const [isTaxOpen, setIsTaxOpen] = useState(false);
+
      return(
           <div className={styles.shopping_summary_container}>
-               kdfnjdbjhfg
+               <div className={styles.container_top}>
+                    <div className={styles.container_top__title}>
+                         <h5>Summary</h5>
+                    </div>
+                    <div className={styles.container_top__tax}>
+                         <div className={styles.top__tax_wrapper}>
+                              <h6>Estimate Shipping and Tax</h6>
+                              <button onClick={() => setIsTaxOpen(prev => !prev)}>
+                                   {!isTaxOpen && <img src={up} alt="up" />}
+                                   {isTaxOpen && <img src={down} alt="down" />}
+                              </button>
+                         </div>
+                         <p>Enter your destination to get a shipping estimate.</p>
+                         {isTaxOpen && <SummaryForm />}
+                    </div>
+                    <div className={styles.container_top__code}></div>
+               </div>
+               <div className={styles.container_main}></div>
+               <div className={styles.container_bottom}></div>
           </div>
      );
 }
---
+++ src/pages/ShoppingCart/components/ShoppingSummary/components/SummaryForm/SummaryForm.jsx
@@ -0,0 +1,48 @@
+import styles from './SummaryForm.module.scss'
+
+import React, { useState } from 'react'
+
+import { countries } from './SummaryForm.helper'
+
+export default function SummaryForm() {
+  const [inputValues, setInputValues] = useState({
+    country: countries[0].name,
+    province: '',
+    zip: ''
+  })
+
+  return (
+    <form action='' className={styles.form}>
+      <div className={styles.top_inputs_wrapper}>
+        <label htmlFor='Contry'>Country</label>
+        <select className={styles.input}>
+          {countries.map(country => (
+            <option key={country.id} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor='State/Province'>State/Province</label>
+        <input type='text' className={styles.input}/>
+        <label htmlFor='Zip/Postal Code'>Zip/Postal Code</label>
+        <input type='number' className={styles.input}/>
+      </div>
+
+      <div className={styles.bottom_inputs_wrapper}>
+        <label htmlFor='Standard Rate'>Standard Rate</label>
+        <div className={styles.wrapper}>
+          <input type='radio' name='option' checked/>
+          <p>Price may vary depending on the item/destination. Shop Staff will
+            contact you. $21.00
+          </p>
+        </div>
+
+        <label htmlFor='Pickup from store'>Pickup from store</label>
+        <div className={styles.wrapper}>
+          <input type='radio' name='option' />
+          <p>1234 Street Adress City Address, 1234 $0.00</p>
+        </div>
+      </div>
+    </form>
+  )
+}
