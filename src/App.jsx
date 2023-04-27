852ac7eec70140ced1c7b8d4f71b7699a134f51c
Bessonov Ilya <illybesson3@gmail.com>
2023-04-27 22:39:57 +0430

    CustomerLogin start

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import TopHeader from './components/common/Header/components/TopHeader'
 import ProductWidget from './components/ProductWidget/ProductWidget'
 import Footer from './components/common/Footer/Footer'
 import News from './components/common/News/News'
+import CustomerLogin from './pages/CustomerLogin/CustomerLogin'
 
 function App() {
   return (
     <div className='App'>
-      <TopHeader />
+      {/* <TopHeader />
       <ProductWidget title='New Products' />
       <News/>
-      <Footer />
+      <Footer /> */}
+      <CustomerLogin />
     </div>
   )
 }
---
+++ src/pages/CustomerLogin/CustomerLogin.jsx
@@ -0,0 +1,20 @@
+import Header from '../../components/common/Header/components/TopHeader'
+import Footer from '../../components/common/Footer/Footer'
+import classes from "./CustomerLogin.module.scss";
+const CustomerLogin = ()=>{
+
+
+  return(
+    <>  
+    <Header/>
+    <div className={classes.root}>
+      <div className={classes.conteiner}>
+        
+      </div>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default CustomerLogin
